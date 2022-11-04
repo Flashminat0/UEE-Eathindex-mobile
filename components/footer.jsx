472e3f2f84c275ef82b7437535a9d88472c1cb5c
W.C.Y.S. Lowe <wyasaslowe@gmail.com>
2022-11-04 19:38:13 +0530

    make changes to footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,8 +2,12 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div className={`fixed bottom-0 left-0 w-screen bg-red-200`}>
-Hi
+    <div className={`fixed bottom-0 left-0 w-screen bg-red-200 grid grid-cols-5`}>
+      <div>Home</div>
+      <div>Search</div>
+      <div>Add</div>
+      <div>comment</div>
+      <div>user</div>
     </div>
   )
 }
