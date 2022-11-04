bf3dfb8392031750e116ebae68dc65cb9c629271
W.C.Y.S. Lowe <wyasaslowe@gmail.com>
2022-11-04 19:50:24 +0530

    footer done

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,13 +1,25 @@
 import React from "react"
+import { AiOutlineHome, AiOutlineSearch, AiOutlinePlus, AiOutlineUser } from "react-icons/ai"
+import { FaRegComment } from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div className={`fixed bottom-0 left-0 w-screen bg-red-200 grid grid-cols-5`}>
-      <div>Home</div>
-      <div>Search</div>
-      <div>Add</div>
-      <div>comment</div>
-      <div>user</div>
+    <div className={`fixed bottom-0 left-0 w-screen h-12 flex justify-around items-center border-t-black`}>
+      <div className={`h-8 flex items-center`}>
+        <AiOutlineHome className={`h-5 w-5 font-bold scale-125`} />
+      </div>
+      <div className={`h-8 flex items-center`}>
+        <AiOutlineSearch className={`h-5 w-5 font-bold scale-125`} />
+      </div>
+      <div className={`h-8  bg-green-300 w-20 rounded-full text-center grid place-items-center scale-125`}>
+        <AiOutlinePlus className={`h-5 w-5 font-bold scale-125`} />
+      </div>
+      <div className={`h-8 flex items-center`}>
+        <FaRegComment className={`h-5 w-5 font-bold scale-125`} />
+      </div>
+      <div className={`h-8 flex items-center`}>
+        <AiOutlineUser className={`h-5 w-5 font-bold scale-125`} />
+      </div>
     </div>
   )
 }
