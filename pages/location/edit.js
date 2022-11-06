68bd2a7481ecd4a6336838e21c5ab465afa1f4ab
Sameera Chathuranga <sameera.chathuranga@atlaslabs.com.au>
2022-11-06 22:16:24 +0530

    bug fixes location

--- pages/location/edit.js
+++ pages/location/edit.js
@@ -177,7 +177,7 @@ const Edit = () => {
                 type="submit"
                 className="inline-flex w-full justify-center items-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
               >
-                Home
+                Update
               </button>
             </div>
           </form>
