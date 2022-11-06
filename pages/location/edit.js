ba18b44254cd15913c38c62d7c679d72924827b3
Sameera Chathuranga <sameera.chathuranga@atlaslabs.com.au>
2022-11-06 22:31:30 +0530

    ui improvements of locaations

--- pages/location/edit.js
+++ pages/location/edit.js
@@ -158,7 +158,7 @@ const Edit = () => {
                   deleteLocation()
                 }}
                 type="button"
-                className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-red-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
               >
                 Delete
               </button>
